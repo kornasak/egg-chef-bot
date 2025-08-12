fa32bdf784920a73ffd7cf7656cfaaf686b0e0b1
KornDva <kornasak@users.noreply.github.com>
2025-08-12 21:00:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,18 +29,18 @@ client.once("ready", async () => {
   console.log(`🤖 Logged in as ${client.user.tag}`);
 
   // ตั้งเวลาส่งข้อความตามมื้อ
-  cron.schedule("0 0 * * *", () => {
-    console.log("Trigger เช้า:", new Date().toISOString());
-    sendMeal("เช้า");
-  });
-  cron.schedule("0 5 * * *", () => {
-    console.log("Trigger กลางวัน:", new Date().toISOString());
-    sendMeal("กลางวัน");
-  });
-  cron.schedule("0 11 * * *", () => {
-    console.log("Trigger เย็น:", new Date().toISOString());
-    sendMeal("เย็น");
-  });
+  //cron.schedule("0 0 * * *", () => {
+  //  console.log("Trigger เช้า:", new Date().toISOString());
+  //  sendMeal("เช้า");
+  //});
+  //cron.schedule("0 5 * * *", () => {
+  //  console.log("Trigger กลางวัน:", new Date().toISOString());
+  //  sendMeal("กลางวัน");
+  //});
+  //cron.schedule("0 11 * * *", () => {
+  //  console.log("Trigger เย็น:", new Date().toISOString());
+  //  sendMeal("เย็น");
+  //});
 
   // ตั้ง status บอท
   const statuses = [
